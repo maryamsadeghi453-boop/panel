d4739b3813fb7027f7bf71e3d7cc49a142c7c889
Mohamadff <maryamsadeghi453@gmail.com>
2026-07-06 10:02:11 +0330

    Modify start.sh to configure Apache with custom port
    Script updates Apache configuration to use a specified port and reinstalls Apache with the prefork MPM.

--- start.sh
+++ start.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
+
+# حذف و نصب مجدد Apache با فقط یک MPM
+apt-get update
+apt-get install -y apache2
+a2dismod mpm_event
+a2dismod mpm_worker
+a2enmod mpm_prefork
+
+# تنظیم پورت Railway
 sed -i "s/Listen 80/Listen ${PORT:-80}/g" /etc/apache2/ports.conf
 sed -i "s/<VirtualHost \*:80>/<VirtualHost *:${PORT:-80}>/g" /etc/apache2/sites-available/000-default.conf
 
+# اجرای Apache
 apache2-foreground
